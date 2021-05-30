9dbc210973af4949906b4611bcf0de5562bb74d9
Nirmal <Nirmal@nkdews.me>
2021-05-30 12:26:10 +0530

    updated Frame initial code

---
+++ run.sh
@@ -0,0 +1,4 @@
+./gradlew assembleDebug
+adb uninstall com.nkdews.quoteframe
+adb install /Users/garden/Downloads/ReLaunchX-master/app/build/outputs/apk/app-debug.apk
+ adb shell am start -n  com.nkdews.quoteframe/.ElectricSignActivity
