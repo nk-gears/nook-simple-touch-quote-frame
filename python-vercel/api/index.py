b3e408c46e9cddd3fa5cda385baf9ff873340f2e
Nirmal <897323+nk-gears@users.noreply.github.com>
2022-06-22 10:12:56 +0530

    updated times

--- python-vercel/api/index.py
+++ python-vercel/api/index.py
@@ -3,7 +3,8 @@ import os
 import requests
 # Get Flask and associated modules
 from flask import Flask, request, render_template, jsonify,request
-
+from datetime import datetime
+import pytz
 # Define the Flask app name from the filename
 
 app = Flask(__name__)
@@ -14,13 +15,21 @@ env = os.environ.get('appenv', 'Development')
 # Begin Routes #
 ################
 
+
+def getTime():
+    indianTZ = pytz.timezone("Asia/Kolkata") 
+    timeinIndia = datetime.now(indianTZ)
+    current_time = timeinIndia.strftime("%H:%M")
+    current_date = timeinIndia.strftime("%d/%m/%Y")
+    return current_date,current_time
+# Output: The current time in New York is: 05:36:59
 # Root
 @app.route('/')
 def default_route():
     resp=requests.get("https://api.quotable.io/random?maxLength=120")
     json=resp.json()
     keyCode = request.args.get('k')
-    tplName="time";
+    tplName="quote";
     if keyCode is None:
         keyCode=""
     else:
